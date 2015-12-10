2a87a9e5e3e93c068a4ae1e0b7b7f679e5c771f5
Kai Dev Hu <kdhu@thoughtworks.com>
2015-12-10 18:46:04 +0800

    init commit.

---
+++ src/main/java/com/thoughtworks/web_recap/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.web_recap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + "Hello World!" + "</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
